9ab5d074efda19bf4766bc323730c0033e306a92
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2021-06-05 13:35:50 -0400

    build POST endpoint for API for creating book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,22 @@ class BooksController < ApplicationController
   def index
     render json: Book.all
   end
+
+  def create
+    # Book.create(title: 'Harry Potter 1', author: 'JK Rowling') # this approach creates it withoutvalidating the parameters
+    book = Book.new(title: params[:title], author: params[:author])  # this creates the object, but doesn't save to DB yet, allows us to validate parameters
+    if book.save
+      render json: book, status: :created # this renders JSON of object back, you could also just say success here too with no body with JSON
+                                 # default success is 200, but we have a more specific one, a 201 response for record successfully created, so we use ``:created` above
+    else
+      render json: book.errors, status: :unproccessable_entity # :unproccessable_entity is for a 422, server understands request, unable to process instructions (parameters were wrong)
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)  # params method is made available by ApplicationController
+    # above allows :title and :author parameters to be POSTed, but no others
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # get '/books' => 'books' # URL mapped to a controller/endpoint.  Add new API endpoint for listing books. hardcoded style
   # nicer way below
-  resources :books, only: :index # `resources` generates all 7 RESTful resources, unless you do the `only: :index` portion
-  # At this point, above is the `:books` controller, and `:index` is the only #action at this time
+  resources :books, only: [:index,:create] # `resources` generates all 7 RESTful resources, unless you do the `only:` part
 end
