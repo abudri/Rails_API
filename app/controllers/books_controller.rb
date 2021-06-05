86da475dbb4cdfc9716c824091399fea446f489d
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2021-06-05 16:26:17 -0400

    add validations for Book model, fix status code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,14 +10,14 @@ class BooksController < ApplicationController
       render json: book, status: :created # this renders JSON of object back, you could also just say success here too with no body with JSON
                                  # default success is 200, but we have a more specific one, a 201 response for record successfully created, so we use ``:created` above
     else
-      render json: book.errors, status: :unproccessable_entity # :unproccessable_entity is for a 422, server understands request, unable to process instructions (parameters were wrong)
+      render json: book.errors, status: :unprocessable_entity # :unprocessable_entity is for a 422, server understands request, unable to process instructions (parameters were wrong)
     end
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title, :author)  # params method is made available by ApplicationController
+    params.require(:book).permit(:author, :title)  # params method is made available by ApplicationController
     # above allows :title and :author parameters to be POSTed, but no others
   end
 end
