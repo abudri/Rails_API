2ef3edc440102b65bdf722508bd819c9cfb22c19
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2021-06-06 07:36:29 -0400

    exception handle for :id not found on a destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,6 +18,8 @@ class BooksController < ApplicationController
     Book.find(params[:id]).destroy!  # ActiveRecord Books model has a method given called `.find` which takes an id. From route book DELETE /books/:id(.:format), books#destroy
                           # destroy!, the ! will return true of successful, and will give an exception if not, which we handle later
     head :no_content # `head` doesn't give JSON, but returns a status code in head of the response but no body, :no_content is a 204 status code
+    rescue ActiveRecord::RecordNotDestroyed
+      render json: {}, status: :unprocessable_entity # now when `.destroy!` a few lines up fails, rather than just giving an error, we capture error and raise a response back to user with status code telling them request they formed was correct, but we were not not able perform the operation
   end
 
   private
