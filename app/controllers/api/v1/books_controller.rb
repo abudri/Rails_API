8f5f85f19aa1e026ff1fd63d9f6691b6adcf7d0e
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2021-06-06 08:52:33 -0400

    namespace and version the API

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,33 @@
+module Api
+  module V1
+    class BooksController < ApplicationController
+      def index
+        render json: Book.all
+      end
+
+      def create
+        # Book.create(title: 'Harry Potter 1', author: 'JK Rowling') # this approach creates it withoutvalidating the parameters
+        book = Book.new(title: params[:title], author: params[:author])  # this creates the object, but doesn't save to DB yet, allows us to validate parameters
+        if book.save
+          render json: book, status: :created # this renders JSON of object back, you could also just say success here too with no body with JSON
+                                    # default success is 200, but we have a more specific one, a 201 response for record successfully created, so we use ``:created` above
+        else
+          render json: book.errors, status: :unprocessable_entity # :unprocessable_entity is for a 422, server understands request, unable to process instructions (parameters were wrong)
+        end
+      end
+
+      def destroy
+        Book.find(params[:id]).destroy!  # ActiveRecord Books model has a method given called `.find` which takes an id. From route book DELETE /books/:id(.:format), books#destroy
+                              # destroy!, the ! will return true of successful, and will give an exception if not, which we handle later
+        head :no_content # `head` doesn't give JSON, but returns a status code in head of the response but no body, :no_content is a 204 status code
+      end
+
+      private
+
+      def book_params
+        params.require(:book).permit(:author, :title)  # params method is made available by ApplicationController
+        # above allows :title and :author parameters to be POSTed, but no others
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get '/books' => 'books' # URL mapped to a controller/endpoint.  Add new API endpoint for listing books. hardcoded style
-  # nicer way below
-  resources :books, only: [:index,:create, :destroy] # `resources` generates all 7 RESTful resources, unless you do the `only:` part
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: [:index,:create, :destroy] # `resources` generates all 7 RESTful resources, unless you do the `only:` part
+    end
+  end
 end
