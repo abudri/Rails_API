86ad241a2a04eb4f1da0518237d84f66269e4c1d
Abdullah Budri <17362519+abudri@users.noreply.github.com>
2021-06-07 06:54:29 -0400

    using `let!` for test object creation

--- spec/requests/books_spec.rb
+++ spec/requests/books_spec.rb
@@ -24,10 +24,12 @@ describe 'Books API', type: :request do  # high level `describe` block, option s
   end
 
   describe 'DELETE /api/v1/books' do  # /api/v1/books 
+    let!(:book) { FactoryBot.create(:book, title: 'The Good Earth', author: 'Pearl Buck') }  # create a book in test database for this test.  use `let` creates the factory and assigns to book, kinda like before
+                # above, withouth the `!` on `let`, the :book is lazy loaded, and isn't there until we hit book.id below.  So `let!` allows us to have 1 book in test DB before test starts
     it 'deletes a book' do
-      book = FactoryBot.create(:book, title: 'The Good Earth', author: 'Pearl Buck')  # create a book in test database for this test
-
-      delete "/api/v1/books/#{book.id}"  # interpolation here with the #{ }, so we don't hardcode the id of 1, in `/api/v1/books/1`, for the book created two lines above
+      expect {
+        delete "/api/v1/books/#{book.id}"  # interpolation here with the #{ }, so we don't hardcode the id of 1, in `/api/v1/books/1`, for the book created two lines above
+      }.to change { Book.count }.from(1).to(0)
 
       expect(response).to have_http_status(:no_content)
     end
